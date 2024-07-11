25e0e758c5b80b9ca4503f6e5f30e603df6c3e23
= <=>
2024-07-11 10:46:22 +0300

    function implemented

---
+++ src/scripts/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function waitFor (element, eventName) {
+    return new Promise((resolve) => {
+        element.addEventListener(eventName, () => {
+            resolve(`It was ${eventName} on the element:
+                    ${element.nodeName}, id: ${element.id}`)
+        })
+    })
+}
+
+function printMessage(message) {
+    const divMessage = document.createElement('div');
+    divMessage.classList.add('message'); 
+    divMessage.innerHTML = message;
+
+    document.body.appendChild(divMessage);
+}
+
+
+const loginField = document.getElementById('login');
+const passwordField = document.getElementById('password');
+const button = document.getElementById('submit');
+
+waitFor(loginField, 'click').then(printMessage);
+waitFor(passwordField, 'click').then(printMessage);
+waitFor(button, 'click').then(printMessage);
+
+waitFor(loginField, 'input').then(printMessage);
+waitFor(passwordField, 'input').then(printMessage);
+
+waitFor(loginField, 'blur').then(printMessage);
+waitFor(passwordField, 'blur').then(printMessage);
+waitFor(button, 'blur').then(printMessage);
